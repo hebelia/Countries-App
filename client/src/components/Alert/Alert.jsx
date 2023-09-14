347fbbb29ff2706c8a08725c0a1d53e905cf3854
hebelia <heberomeu@gmail.com>
2023-09-14 01:51:39 -0300

    scss adjustments

--- client/src/components/Alert/Alert.jsx
+++ client/src/components/Alert/Alert.jsx
@@ -1,34 +1,39 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import { useRef } from "react";
+import { useRef } from "react"; // useRef hook from React.
 
-import imgOK from "/img/ok_Icon.png";
-import imgError from "/img/error_icon.png";
+import imgOK from "/img/ok_Icon.png"; // for the "success" type.
+import imgError from "/img/error_icon.png"; //  for the "error" type.
 
-import style from "./Alert.module.scss";
-import styleBackground from "../../views/ActivityForm/ActivityForm.module.scss";
+import style from "./Alert.module.scss"; // Import CSS module styles for the Alert component.
+import styleBackground from "../../views/ActivityForm/ActivityForm.module.scss"; // Import CSS module styles for background (used in ActivityForm).
+
 const Alert = ({ title, text, textBTN, background, type }) => {
-  const alert = useRef();
+  // Define the Alert functional component with props: title, text, textBTN, background, and type.
+  const alert = useRef(); // Create a ref for the alert element.
 
   const handleOnClick = (e) => {
-    e.preventDefault();
-    background.current.classList.toggle(styleBackground.showVisibility);
-    alert.current.classList.toggle(`${style.openPopUp}`);
+    // Define a click event handler for the button.
+    e.preventDefault(); // Prevent the default behavior of anchor tags (prevents page reload).
+
+    background.current.classList.toggle(styleBackground.showVisibility); // Toggle the visibility of the background element.
+    alert.current.classList.toggle(`${style.openPopUp}`); // Toggle the open state of the alert popup.
   };
 
   return (
     <div ref={alert} className={style.popup}>
+      {/* Render the alert popup with conditional content based on the "type" prop. */}
       <img
         className={style.img}
-        src={type === "success" ? imgOK : imgError}
+        src={type === "success" ? imgOK : imgError} // Display the appropriate icon based on the "type" prop.
         alt={"Icon"}
       />
-      <h3 className={style.title}>{title}</h3>
-      <p>{text}</p>
+      <h3 className={style.title}>{title}</h3> {/* Render the alert title. */}
+      <p>{text}</p> {/* Render the alert text. */}
       <button onClick={handleOnClick} className={style.btn} href="#">
         {textBTN}
-      </button>
+      </button>{" "}
+      {/* Render a button with custom text and a click event handler. */}
     </div>
   );
 };
 
-export default Alert;
+export default Alert; // Export the Alert component for use in other parts of the application.
--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,13 +1,21 @@
 import style from "./Loading.module.scss";
+import logo from "/img/logo2.png";
 
 const Loading = () => {
   return (
     <div className={style.background}>
       <div className={style.containerLoading}>
-        <div className={`${style.globeLoader} fa-solid fa-globe`}>
-          <i className="fas fa-plane"></i>
-          <p>LOADING..</p>
+        <div className={style.globeLoader}>
+          <img className={style.img} src={logo} alt="Logo" />
+          <h4>Loading... well this is embarrassing...</h4>
         </div>
+        {/* <div class={style.loading}>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div> */}
       </div>
     </div>
   );
